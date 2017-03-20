7deee71b6592cd3ef39e52e5745bb5f9c7b69778
Cameron Milsom <cameron.milsom@gmail.com>
2017-03-20 21:29:53 +0000

    Now has a route for retrieving the favicon. Needs some visibility improvements at a later stage.

--- fittings-server/src/bin/main.rs
+++ fittings-server/src/bin/main.rs
@@ -6,11 +6,20 @@ extern crate fittings_server;
 
 use fittings_server::examples;
 
+//ZZZ TODO Move static files out
+use std::path::Path;
+use rocket::response::NamedFile;
+
+#[get("/favicon.ico")]
+fn get_fav_icon() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/favicon.png")).ok()
+}
+
 fn main() {
     let fittings_rocket = rocket::ignite();
 
-    let routes = examples::get_routes("/");//(fittings_rocket, "/");
+    let mut routes = examples::get_routes("/");
+    routes.extend(routes![get_fav_icon]);
 
     fittings_rocket.mount("/", routes).launch();
-    // rocket::ignite().launch()
 }
