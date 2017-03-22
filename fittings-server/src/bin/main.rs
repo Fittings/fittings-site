764fe56644d1837b8c488a821bfa93f7fb044f71
Cameron Milsom <cameron.milsom@gmail.com>
2017-03-22 20:59:48 +0000

    More changes to the styling and css. Now supports actually displaying content nicely.

--- fittings-server/src/bin/main.rs
+++ fittings-server/src/bin/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let fittings_rocket = rocket::ignite();
 
     let mut routes = examples::get_routes("/");
-    routes.extend(routes![index, get_fav_icon, get_main_css]);
+    routes.extend(routes![index, get_fav_icon, get_static_files]);
 
     fittings_rocket.mount("/", routes).launch();
 }
@@ -20,20 +20,25 @@ fn main() {
 
 //ZZZ TODO Move static files out
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use rocket::response::NamedFile;
 
+
+#[get("/<file..>")]
+fn get_static_files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
 
 #[get("/favicon.ico")]
 fn get_fav_icon() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/favicon.png")).ok()
 }
 
-#[get("/main.css")]
-fn get_main_css() -> io::Result<NamedFile> {
-    NamedFile::open("static/main.css")
-}
-
+// #[get("/main.css")]
+// fn get_main_css() -> io::Result<NamedFile> {
+//     NamedFile::open("static/main.css")
+// }
+//
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
