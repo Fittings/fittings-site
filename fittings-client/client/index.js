2e7885b668319afbeb2b0fa70213d328435e88d0
Cameron Milsom <cameron.milsom@gmail.com>
2017-04-04 20:48:43 +0100

    Added in basic Hello World REACT front end. Needs to be built using: yarn build. Which is set in the package.json file

---
+++ fittings-client/client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+console.log("Hello World!")
+ReactDOM.render(<App />, document.getElementById('root'));
