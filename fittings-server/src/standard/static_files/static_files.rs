70bc208a1d92e9b43db49c67617489201093dcdf
Cameron Milsom <cameron.milsom@gmail.com>
2017-04-11 20:46:59 +0100

    Updated server to handle http reqeusts for general static files. Fixed issue with routing when using invalid query param.

--- fittings-server/src/standard/static_files/static_files.rs
+++ fittings-server/src/standard/static_files/static_files.rs
@@ -5,29 +5,31 @@ use rocket::response::NamedFile;
 
 
 pub fn mount(rocket: rocket::Rocket, base_address: &str) -> rocket::Rocket {
-    rocket.mount(base_address, routes![get_client_files, get_static_files, get_fav_icon, index])
+    rocket.mount(base_address, routes![get_static_files, index])
 }
 
 
-#[get("/static/<file..>")]
-fn get_static_files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
-}
 
-
-#[get("/client/public/<file..>")]
-fn get_client_files(file: PathBuf) -> Option<NamedFile> {
+#[get("/<file..>?<_anything>")]
+fn get_static_files(file: PathBuf, _anything: Ignored) -> Option<NamedFile> {
     NamedFile::open(Path::new("client/public/").join(file)).ok()
-}
-
-
-#[get("/favicon.ico")]
-fn get_fav_icon() -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/favicon.ico")).ok()
 }
 
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("client/public/index.html")
+}
+
+
+
+//Structure used for ignoring URL params. See: {https://github.com/SergioBenitez/Rocket/issues/219#issuecomment-285257085}
+struct Ignored;
+
+impl<'f> rocket::request::FromForm<'f> for Ignored {
+    type Error = ();
+
+    fn from_form_items(_items: &mut rocket::request::FormItems<'f>) -> Result<Self, Self::Error> {
+        Ok(Ignored)
+    }
 }
