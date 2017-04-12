9c9c7cd635b750317a53fc531f9b853dd7dcfee4
Cameron Milsom <cameron.milsom@gmail.com>
2017-04-12 21:00:10 +0100

    Added in a dodgy 404

--- fittings-client/src/components/app.js
+++ fittings-client/src/components/app.js
@@ -7,11 +7,18 @@ class App extends Component {
         <Router history={hashHistory}>
           <Route path='/' component={Home} />
           <Route path='/cameron-milsom' component={CameronMilsom} />
+          <Route path="/*" component={NotFound} />
         </Router>)
   }
 }
 
 const Home = () => <h1>This is my home page...</h1>
 const CameronMilsom = () => <span>Cameron Milsom</span>
+const NotFound = () => (<div><h1>Not Found!</h1>
+                        <span>This page doesn't exist. Maybe it should exist?</span>
+                        <span>Vist my </span>
+                        <a href="https://github.com/Fittings/fittings-site">GitHub</a>
+                        <span> and raise an issue.</span>
+                        </div>)
 
 export default App
