6f46168c2f6a1122ccc41c05ae301ffbd824cda9
Cameron Milsom <cameron.milsom@gmail.com>
2017-04-13 20:21:33 +0100

    Minor changes to routing. Now uses browserHisotry instead of hashHistory

--- fittings-client/src/app-client.js
+++ fittings-client/src/app-client.js
@@ -19,5 +19,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
+import Header from './components/standard/header'
 
+// ReactDOM.render(<Header />, document.getElementById('body'))
 ReactDOM.render(<App />, document.getElementById('root'))
--- fittings-client/src/components/app.js
+++ fittings-client/src/components/app.js
@@ -4,15 +4,16 @@ import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 're
 class App extends Component {
   render() {
     return (
-        <Router history={hashHistory}>
-          <Route path='/' component={Home} />
-          <Route path='/cameron-milsom' component={CameronMilsom} />
-          <Route path="/*" component={NotFound} />
+        <Router history={browserHistory}>
+          <Route path='/' component={Home}>
+            <Route path='cameron-milsom' component={CameronMilsom} />
+            <Route path="*" component={NotFound} />
+          </Route>
         </Router>)
   }
 }
 
-const Home = () => <h1>This is my home page...</h1>
+const Home = () => <h1>This is my home page..?</h1>
 const CameronMilsom = () => <span>Cameron Milsom</span>
 const NotFound = () => (<div><h1>Not Found!</h1>
                         <span>This page doesn't exist. Maybe it should exist?</span>
