b667892f4b1ccdf2ebba6050b3b09e9441d933f5
Cameron Milsom <cameron.milsom@gmail.com>
2017-04-16 19:25:23 +0100

    Re-started development again. I know! Big learning process trying to understand how front end works.

--- fittings-client/src/app-client.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/app'
-import Header from './components/standard/header'
-
-
-// ReactDOM.render(<Header />, document.getElementById('body'))
-ReactDOM.render(<App />, document.getElementById('root'))
