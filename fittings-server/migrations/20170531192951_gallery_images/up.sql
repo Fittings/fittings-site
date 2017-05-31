219e0d8d8e72c725cbd667cec59b48d1f1317d76
Fittings <cameron.live@gmail.com>
2017-05-31 20:38:29 +0100

    Added in tables to support galleries.

---
+++ fittings-server/migrations/20170531192951_gallery_images/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE gallery_images(
+  gallery_id INTEGER NOT NULL,
+  image_id INTEGER NOT NULL,
+  FOREIGN KEY(gallery_id) REFERENCES galleries(id),
+  FOREIGN KEY(image_id) REFERENCES image_locations(id)
+);
