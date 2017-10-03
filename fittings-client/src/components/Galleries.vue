f0e04a116e67122586b5ba2aecaad747a8f26831
Cameron Milsom <cameron.milsom@gmail.com>
2017-10-03 20:08:50 +0100

    Galleries: - Updated (UI): Added in material style cards.

--- fittings-client/src/components/Galleries.vue
+++ fittings-client/src/components/Galleries.vue
@@ -3,19 +3,24 @@
     <div class="hero">
       <img class="hero-image" v-bind:src="heroPreviewUrl"/>
       <div class="hero-text">
-        <h1>My Photos</h1>
-        <p><em>From places</em></p>
+        <h1>Photos</h1>
+        <p><em>From wherever.</em></p>
       </div>
     </div>
     <div class="main-content">
       <div class="row gallery-row" v-for="(gallery, index) in galleries" v-bind:key="index">
         <div class="column">
           <a class="gallery-border">
-            <img class="gallery-preview" v-bind:src="gallery.preview_url" v-bind:alt="gallery.description"/>
-            <section class="gallery-description">
-              <h2>{{ gallery.name }}</h2>
-              <p><em>{{ gallery.description }}</em></p>
-            </section>
+            <div class="card">
+              <div class="card-image">
+                <img class="gallery-preview" v-bind:src="gallery.preview_url" v-bind:alt="gallery.description"/>
+              </div>
+              <div class="card-content">
+                <h2>{{ gallery.name }}</h2>
+                <p><em>{{ gallery.description }}</em></p>
+              </div>              
+              </section>
+            </div>
           </a>
         </div>
       </div>
@@ -56,21 +61,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .hero {
-    margin-bottom: 2rem;
-    box-shadow: 0 -1000px rgba(255, 255, 255, 1) inset;
-    box-shadow: 0 5px -10px rgba(182, 182, 182, 0.75);
+    width: 100%;
   }
+
 
   .hero-image {
-    filter: grayscale(80%) brightness(0.8) blur(0.05rem);;
+    filter: grayscale(40%) brightness(0.8) blur(0.05rem);;
     width: 100%;
-    height: 35rem;
+    height: 60rem;
     object-fit: cover;
-    border-bottom: 100px white;
+    -webkit-mask-image: -webkit-gradient(linear, left 90%, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
     /* box-shadow: 0 4px 100px -100px white; */
+    border-bottom: 1px solid rgba(0, 0, 0, 1);
+
   }
-
-
 
   .hero-text {
     text-align: center;
@@ -80,17 +84,25 @@ export default {
     transform: translate(0%, 100%);
     width: 100%;
     color: white;
-    /* offset-x | offset-y | blur-radius | color */
     text-shadow: 0.1rem 0.1rem 0.5px black;
-    font-weight: 300;
+    font-weight: 100;
+    }
+
+  .hero-text > h1 {
+    font-size: 10rem;
+    font-weight: 800;
+    margin-bottom: 0;
   }
 
-
+  .main-content {
+    margin-top: -10rem;
+  }
+  .main-content * {
+    z-index: 1;
+  }
 
   .gallery-row {
-    margin-bottom: 3rem;
   }
-
 
   .gallery-border {
     cursor: pointer;
@@ -101,24 +113,38 @@ export default {
     color: red;
   }
 
+  .gallery-preview-blur {
+    position: absolute;
+  }
   .gallery-preview {
+    margin: 0 0 0 0;
     filter: saturate(90%) ;
     width: 100%;
     height: 350px;
-    border-radius: 0.6rem;
     object-fit: cover;
     z-index: -1;
 
   }
 
+  .card:hover {
+    box-shadow:0 3px 3px 0 rgba(0,0,0,0.14),0 1px 7px 0 rgba(0,0,0,0.12),0 3px 1px -1px rgba(0,0,0,0.2)
+  }
   .column:hover .gallery-preview {
     filter: saturate(110%) brightness(1.1);
   }
 
-
+  .gallery-description a {
+    color: black !important;
+  }
   .gallery-description > h2 {
     margin-top: 0;
+    margin-bottom: 0;
   }
+
+  .gallery-description > p {
+    margin-top: 0;
+  }
+
 
 
 </style>
